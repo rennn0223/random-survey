5ce3770928d39fee3439792b2f698fb9aa0030fd
rennn0223 <123361817+rennn0223@users.noreply.github.com>
2026-02-18 20:23:05 +0800

    Update URLs in url.js
    Removed old URLs and added new ones for NCHU.

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.nchu.edu.tw/calendar/',
+	'https://lms2020.nchu.edu.tw',
 ];
